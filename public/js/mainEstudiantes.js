c914ae785926c8051cb538fb0fb3fdc00063af59
genoNC <calebcastillo2001fwd@gmail.com>
2025-08-27 09:16:18 -0600

    boton guardar pagina main estudiante

--- public/js/mainEstudiantes.js
+++ public/js/mainEstudiantes.js
@@ -1,16 +1,26 @@
 const nuevaconsulta =document.getElementById("consulta")
 const btnGuardar =document.getElementById("btnGuardar")
 const histConsultas =document.getElementById("histConsultas")
+const profesor =document.getElementById("profesor")
 
 
 const fechaActual = Date.now()
 import { postUsuarios } from "../services/servicesProduc.js"
+
+
 btnGuardar.addEventListener("click", async function () {
+   if (nuevaconsulta.value != "") {
+    
     const duda={ 
         consulta: nuevaconsulta.value,
         fecha: fechaActual,
-        estado: true
+        estado: true,
+        profesor: profesor.value
     }
 
-    const respuesta = await post(movie)
+    const respuesta = await postUsuarios(duda)  
+} else{
+    Swal.fire('Error al guardar', 'Debe ingresar una tarea', 'error');
+}
 })
+
