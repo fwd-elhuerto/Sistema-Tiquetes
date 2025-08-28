12667c77bf0c4de773e7cb0f83328222d342feb6
RexCuso <rogerch2011@gmail.com>
2025-08-28 10:32:50 -0600

    registro y login funcional

---
+++ public/js/registro.js
@@ -0,0 +1,44 @@
+
+/* constantes a usar  */
+/*UN= Nombre de usuario/ UC= Contraseña de usuario/ UT= Telefono de usuario/SD= sede de usuario*/
+const UN =document.getElementById("UN")
+const UC =document.getElementById("UC")
+const UT =document.getElementById("UT")
+const SD =document.getElementById("SD")
+const btRegistro =document.getElementById("btRegistro")
+
+/* funcion llamada desde el servises */
+import { getUsuarios,postUsuarios } from "../services/servicesUsuarios.js"
+
+
+
+/* el boton para guardar los usuarios  */
+btRegistro.addEventListener("click", async function getUsuarios() {
+    /* crear una constante que guarde las carateristicas de los usuario (un objeto) */
+    if (UN !="" && UC !="" && UT!="" ) {
+        const usuario= {
+        usuario: UN.value,
+        password: UC.value,
+        telefono: UT.value,
+        sede:SD.value
+    }
+    /* constante que llama la funcion para subir los usuarios */
+    const DUsuarios = await postUsuarios(usuario)
+    console.log(DUsuarios);
+    window.location.href = "login.html";
+        
+    } else {
+        Swal.fire('Error al ingresar', 'Llene Todos Los Campos Solisitados', 'error');
+        
+    }
+    
+})
+
+/* fucion para limpiar el formulario  */
+function limpiar() {
+    UN.textContent="" 
+    UC.textContent=""
+    UT.textContent=""
+}
+
+limpiar()
